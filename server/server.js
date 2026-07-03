c915338b54c998c851aee74893be76adf33dc051
elbse <changpriego@gmail.com>
2026-07-04 00:07:46 +0800

    feat: implement JWT authentication with user registration and login

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import authRoutes from './routes/auth.js'
 
 dotenv.config()
 
@@ -11,6 +12,8 @@ connectDB()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Trackr API running' })
